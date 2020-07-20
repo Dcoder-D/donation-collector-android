374231ce5aae233198e23081ee41b56ef3f7bd0e
Yichi Zhang <zhangyichi9604@gmail.com>
2020-07-19 19:31:18 -0500

    add AppUser to database

--- app/src/main/java/com/flagcamp/donationcollector/database/AppDatabase.java
+++ app/src/main/java/com/flagcamp/donationcollector/database/AppDatabase.java
@@ -7,8 +7,9 @@ import androidx.room.TypeConverters;
 
 import com.flagcamp.donationcollector.model.Item;
 import com.flagcamp.donationcollector.repository.Converters;
+import com.flagcamp.donationcollector.signin.AppUser;
 
-@Database(entities = {Item.class}, version = 1)
+@Database(entities = {Item.class, AppUser.class}, version = 1)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract  RoomDao dao();
