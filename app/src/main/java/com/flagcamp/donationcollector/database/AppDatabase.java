64f37f1186480585532ce330f6940ef5477c0b24
Dcoder-D <steven.dingnyu@gmail.com>
2020-07-28 22:17:43 -0400

    Complete the createPost Api, getPostsUsers, getPostNGO api connection to backend server.

--- app/src/main/java/com/flagcamp/donationcollector/database/AppDatabase.java
+++ app/src/main/java/com/flagcamp/donationcollector/database/AppDatabase.java
@@ -6,11 +6,15 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 import com.flagcamp.donationcollector.model.Item;
+import com.flagcamp.donationcollector.model.Location;
 import com.flagcamp.donationcollector.repository.Converters;
+import com.flagcamp.donationcollector.repository.NGOUserConverters;
+import com.flagcamp.donationcollector.repository.PosterUserConverters;
+import com.flagcamp.donationcollector.repository.PosterUserReceivedConverters;
 import com.flagcamp.donationcollector.signin.AppUser;
 
 @Database(entities = {Item.class, AppUser.class}, version = 1)
-@TypeConverters({Converters.class})
+@TypeConverters({Converters.class, PosterUserConverters.class, NGOUserConverters.class, PosterUserReceivedConverters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract RoomDao dao();
 }
